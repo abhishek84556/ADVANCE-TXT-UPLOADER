6fb92eb18c9f6d0afba5adf1629404407841c69b
Abhi Dev <168731980+abhishek84556@users.noreply.github.com>
2025-10-24 05:36:56 +0530

    Change API credentials and update author credit
    Updated API credentials and author credit.
    
    Signed-off-by: Abhi Dev <168731980+abhishek84556@users.noreply.github.com>

--- vars.py
+++ vars.py
@@ -1,8 +1,8 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
+# Don't Remove Credit Tg - @abhi
+# Ask Doubt on telegram @abhi
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "24473318")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7832139892:AAFBRTPblJR6H-cWl7E6i6zFtP_4gfrm9Uo") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "29028535") #Replace with your api id
+API_HASH = environ.get("API_HASH", "9e501cebfbd5da42d6b74d48a1bf2536") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "7839271598:AAEVSyu_tfcxT8Jk4TvGdEb0Up3VwjBJKig") #Replace with your bot token
